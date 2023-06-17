8d401db5dc731b26c97554d167c0ee92e1d316f9
DraedenJensen <draeden.waffles@gmail.com>
2023-06-17 13:54:02 -0600

    Changed the name of the namespace.

--- ChessModel/Chessboard.cs
+++ ChessModel/Chessboard.cs
@@ -1,4 +1,4 @@
-﻿namespace ChessModel
+﻿namespace ChessModels
 {
     public class Chessboard
     {
--- ChessModelTest/UnitTest1.cs
+++ ChessModelTest/UnitTest1.cs
@@ -1,4 +1,4 @@
-using ChessModel;
+using ChessModels;
 using System.Diagnostics;
 
 namespace ChessModelsTest
