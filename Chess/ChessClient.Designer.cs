8d49bed24bd901b6f8a573b6001561caaed91012
DraedenJensen <draeden.waffles@gmail.com>
2023-10-14 14:32:06 -0600

    Starting setting up the settings menu

--- Chess/ChessClient.Designer.cs
+++ Chess/ChessClient.Designer.cs
@@ -34,8 +34,30 @@
             button2 = new Button();
             pictureBox1 = new PictureBox();
             pictureBox2 = new PictureBox();
+            mainMenu = new GroupBox();
+            multiplayerSettings = new GroupBox();
+            pictureBox3 = new PictureBox();
+            checkBox3 = new CheckBox();
+            checkBox2 = new CheckBox();
+            checkBox1 = new CheckBox();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            gray = new RadioButton();
+            green = new RadioButton();
+            wood = new RadioButton();
+            marble = new RadioButton();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            startMultiplayerGame = new Button();
+            backButton = new Button();
+            label2 = new Label();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            mainMenu.SuspendLayout();
+            multiplayerSettings.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            flowLayoutPanel1.SuspendLayout();
             SuspendLayout();
             // 
             // label1
@@ -43,7 +65,7 @@
             label1.Anchor = AnchorStyles.None;
             label1.BackColor = Color.Transparent;
             label1.Font = new Font("Imprint MT Shadow", 48F, FontStyle.Underline, GraphicsUnit.Point);
-            label1.Location = new Point(181, 67);
+            label1.Location = new Point(82, 41);
             label1.Name = "label1";
             label1.Size = new Size(474, 94);
             label1.TabIndex = 0;
@@ -55,7 +77,7 @@
             button1.Anchor = AnchorStyles.None;
             button1.BackColor = Color.DarkGray;
             button1.Font = new Font("Bell MT", 18F, FontStyle.Regular, GraphicsUnit.Point);
-            button1.Location = new Point(181, 214);
+            button1.Location = new Point(82, 179);
             button1.Name = "button1";
             button1.Size = new Size(474, 75);
             button1.TabIndex = 1;
@@ -68,7 +90,7 @@
             button2.Anchor = AnchorStyles.None;
             button2.BackColor = Color.DarkGray;
             button2.Font = new Font("Bell MT", 18F, FontStyle.Regular, GraphicsUnit.Point);
-            button2.Location = new Point(181, 342);
+            button2.Location = new Point(82, 308);
             button2.Name = "button2";
             button2.Size = new Size(474, 75);
             button2.TabIndex = 2;
@@ -92,29 +114,245 @@
             pictureBox2.BackgroundImage = ChessClientGUI.Properties.Resources.blackPillar3;
             pictureBox2.BackgroundImageLayout = ImageLayout.Stretch;
             pictureBox2.Dock = DockStyle.Right;
-            pictureBox2.Location = new Point(661, 0);
+            pictureBox2.Location = new Point(849, 0);
             pictureBox2.Name = "pictureBox2";
             pictureBox2.Size = new Size(175, 553);
             pictureBox2.TabIndex = 4;
             pictureBox2.TabStop = false;
             // 
+            // mainMenu
+            // 
+            mainMenu.Controls.Add(label1);
+            mainMenu.Controls.Add(button1);
+            mainMenu.Controls.Add(button2);
+            mainMenu.Location = new Point(193, 44);
+            mainMenu.Name = "mainMenu";
+            mainMenu.Size = new Size(639, 465);
+            mainMenu.TabIndex = 5;
+            mainMenu.TabStop = false;
+            // 
+            // multiplayerSettings
+            // 
+            multiplayerSettings.Controls.Add(pictureBox3);
+            multiplayerSettings.Controls.Add(checkBox3);
+            multiplayerSettings.Controls.Add(checkBox2);
+            multiplayerSettings.Controls.Add(checkBox1);
+            multiplayerSettings.Controls.Add(flowLayoutPanel1);
+            multiplayerSettings.Controls.Add(label6);
+            multiplayerSettings.Controls.Add(label5);
+            multiplayerSettings.Controls.Add(label4);
+            multiplayerSettings.Controls.Add(label3);
+            multiplayerSettings.Controls.Add(startMultiplayerGame);
+            multiplayerSettings.Controls.Add(backButton);
+            multiplayerSettings.Controls.Add(label2);
+            multiplayerSettings.Location = new Point(199, 25);
+            multiplayerSettings.Name = "multiplayerSettings";
+            multiplayerSettings.Size = new Size(627, 516);
+            multiplayerSettings.TabIndex = 3;
+            multiplayerSettings.TabStop = false;
+            multiplayerSettings.Visible = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackgroundImage = ChessClientGUI.Properties.Resources.line;
+            pictureBox3.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox3.Location = new Point(29, 77);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(568, 41);
+            pictureBox3.TabIndex = 11;
+            pictureBox3.TabStop = false;
+            // 
+            // checkBox3
+            // 
+            checkBox3.Appearance = Appearance.Button;
+            checkBox3.Location = new Point(493, 304);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(30, 30);
+            checkBox3.TabIndex = 10;
+            checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.Appearance = Appearance.Button;
+            checkBox2.Location = new Point(493, 244);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(30, 30);
+            checkBox2.TabIndex = 9;
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            checkBox1.Appearance = Appearance.Button;
+            checkBox1.Location = new Point(493, 184);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(30, 30);
+            checkBox1.TabIndex = 8;
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.AutoSize = true;
+            flowLayoutPanel1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            flowLayoutPanel1.Controls.Add(gray);
+            flowLayoutPanel1.Controls.Add(green);
+            flowLayoutPanel1.Controls.Add(wood);
+            flowLayoutPanel1.Controls.Add(marble);
+            flowLayoutPanel1.Location = new Point(205, 126);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(321, 31);
+            flowLayoutPanel1.TabIndex = 7;
+            // 
+            // gray
+            // 
+            gray.Appearance = Appearance.Button;
+            gray.AutoSize = true;
+            gray.Font = new Font("Bell MT", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            gray.Location = new Point(1, 1);
+            gray.Margin = new Padding(1);
+            gray.Name = "gray";
+            gray.Size = new Size(94, 29);
+            gray.TabIndex = 0;
+            gray.TabStop = true;
+            gray.Text = "Simple gray";
+            gray.UseVisualStyleBackColor = true;
+            // 
+            // green
+            // 
+            green.Appearance = Appearance.Button;
+            green.AutoSize = true;
+            green.Font = new Font("Bell MT", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            green.Location = new Point(97, 1);
+            green.Margin = new Padding(1);
+            green.Name = "green";
+            green.Size = new Size(99, 29);
+            green.TabIndex = 1;
+            green.TabStop = true;
+            green.Text = "Simple green";
+            green.UseVisualStyleBackColor = true;
+            // 
+            // wood
+            // 
+            wood.Appearance = Appearance.Button;
+            wood.AutoSize = true;
+            wood.Font = new Font("Bell MT", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            wood.Location = new Point(198, 1);
+            wood.Margin = new Padding(1);
+            wood.Name = "wood";
+            wood.Size = new Size(56, 29);
+            wood.TabIndex = 2;
+            wood.TabStop = true;
+            wood.Text = "Wood";
+            wood.UseVisualStyleBackColor = true;
+            // 
+            // marble
+            // 
+            marble.Appearance = Appearance.Button;
+            marble.AutoSize = true;
+            marble.Font = new Font("Bell MT", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            marble.Location = new Point(256, 1);
+            marble.Margin = new Padding(1);
+            marble.Name = "marble";
+            marble.Size = new Size(64, 29);
+            marble.TabIndex = 3;
+            marble.TabStop = true;
+            marble.Text = "Marble";
+            marble.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Bell MT", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.Location = new Point(103, 304);
+            label6.Name = "label6";
+            label6.Size = new Size(395, 34);
+            label6.TabIndex = 6;
+            label6.Text = "Full Screen . . . . . . . . . . . . . . . .";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Bell MT", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(103, 244);
+            label5.Name = "label5";
+            label5.Size = new Size(397, 34);
+            label5.TabIndex = 5;
+            label5.Text = "Show available moves . . . . . . . ";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Bell MT", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(103, 184);
+            label4.Name = "label4";
+            label4.Size = new Size(406, 34);
+            label4.TabIndex = 4;
+            label4.Text = "Flip board on black's turn . . . . .";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Bell MT", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(100, 124);
+            label3.Name = "label3";
+            label3.Size = new Size(102, 34);
+            label3.TabIndex = 3;
+            label3.Text = "Theme";
+            // 
+            // startMultiplayerGame
+            // 
+            startMultiplayerGame.BackColor = Color.DarkGray;
+            startMultiplayerGame.Font = new Font("Bell MT", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            startMultiplayerGame.Location = new Point(179, 364);
+            startMultiplayerGame.Name = "startMultiplayerGame";
+            startMultiplayerGame.Size = new Size(269, 67);
+            startMultiplayerGame.TabIndex = 2;
+            startMultiplayerGame.Text = "Start Game";
+            startMultiplayerGame.UseVisualStyleBackColor = false;
+            // 
+            // backButton
+            // 
+            backButton.BackColor = Color.White;
+            backButton.Font = new Font("Bell MT", 13.8F, FontStyle.Regular, GraphicsUnit.Point);
+            backButton.Location = new Point(229, 437);
+            backButton.Name = "backButton";
+            backButton.Size = new Size(168, 45);
+            backButton.TabIndex = 1;
+            backButton.Text = "Back to menu";
+            backButton.UseVisualStyleBackColor = false;
+            backButton.Click += backButton_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Imprint MT Shadow", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(65, 3);
+            label2.Name = "label2";
+            label2.Size = new Size(497, 71);
+            label2.TabIndex = 0;
+            label2.Text = "Multiplayer Game";
+            // 
             // ChessClient
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.WhiteSmoke;
+            BackColor = Color.Gainsboro;
             BackgroundImageLayout = ImageLayout.Zoom;
-            ClientSize = new Size(836, 553);
+            ClientSize = new Size(1024, 553);
+            Controls.Add(multiplayerSettings);
             Controls.Add(pictureBox2);
             Controls.Add(pictureBox1);
-            Controls.Add(button2);
-            Controls.Add(button1);
-            Controls.Add(label1);
+            Controls.Add(mainMenu);
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "ChessClient";
             Text = "Chess";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            mainMenu.ResumeLayout(false);
+            multiplayerSettings.ResumeLayout(false);
+            multiplayerSettings.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            flowLayoutPanel1.ResumeLayout(false);
+            flowLayoutPanel1.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -125,5 +363,23 @@
         private Button button2;
         private PictureBox pictureBox1;
         private PictureBox pictureBox2;
+        private GroupBox mainMenu;
+        private GroupBox multiplayerSettings;
+        private Button backButton;
+        private Label label2;
+        private Button startMultiplayerGame;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private RadioButton gray;
+        private RadioButton green;
+        private RadioButton wood;
+        private RadioButton marble;
+        private CheckBox checkBox3;
+        private CheckBox checkBox2;
+        private CheckBox checkBox1;
+        private PictureBox pictureBox3;
     }
 }
--- Chess/ChessClient.cs
+++ Chess/ChessClient.cs
@@ -11,12 +11,39 @@ namespace Chess
 
         private void StartMultiplayerGame(object sender, EventArgs e)
         {
+            mainMenu.Visible = false;
+            multiplayerSettings.Visible = true;
             this.Hide();
             ChessGame game = new ChessGame();
             game.ShowDialog();
         }
 
         private void startComputerGame(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
         {
 
         }
